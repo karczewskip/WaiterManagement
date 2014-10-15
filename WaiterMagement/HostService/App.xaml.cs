99289f4d814c4f824f139c0a2dd547f79aaada98
Pawel <pawel.karczewski@lingaro.com>
2014-10-15 09:13:54 +0200

    HostService jedynie prowizorycznie

---
+++ WaiterMagement/HostService/App.xaml.cs
@@ -0,0 +1,35 @@
+﻿using Ninject;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using HostService.Infrastructure;
+using HostService.Abstract;
+
+namespace HostService
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private IKernel container;
+        private IMainWindow MainWindow;
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            Infrastructure.Installers.ConfigureContainer(out container);
+            ComposeObjects();
+            MainWindow.Show();
+        }
+
+        private void ComposeObjects()
+        {
+            MainWindow = this.container.Get<IMainWindow>();
+        }
+    }
+}
