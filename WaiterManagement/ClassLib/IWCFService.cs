108791947de374c53bc2954e74a27ca068a6e69c
ILoveCamelCasing <davidcvpl@gmail.com>
2014-10-18 13:30:04 +0200

    Poprawka, niektore pliki się nie chciały commitować

---
+++ WaiterManagement/ClassLib/IWCFService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WcfServiceLib
+{
+    [ServiceContract]
+    public interface IWCFService
+    {
+        [OperationContract]
+        WaiterContext LogIn(string login, string pass);
+
+        [OperationContract]
+        bool LogOut(WaiterContext waiterContext);
+
+        [OperationContract]
+        IEnumerable<MenuItem> GetMenu();
+
+        [OperationContract]
+        IEnumerable<MenuItem> GetOrders();
+
+        [OperationContract]
+        IEnumerable<Table> GetTables();
+    }
+}
--- WaiterManagement/HostService_doprzerobienia/Properties/AssemblyInfo.cs
+++ WaiterManagement/HostService_doprzerobienia/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("HostService")]
+[assembly: AssemblyTitle("HostService_doprzerobienia")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("HostService")]
+[assembly: AssemblyProduct("HostService_doprzerobienia")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
