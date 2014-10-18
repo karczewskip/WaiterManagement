29525ea7758d179867e53ab8acd086fb4f3cc819
Pawel <pawel.karczewski@lingaro.com>
2014-10-18 19:22:50 +0200

    Zabawa z Gitem - Naprawa błędów

---
+++ WaiterManagement/BarManager/View/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using BarManager.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BarManager.View
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, IMainWindow
+    {
+        IMenuManager MenuManager;
+
+        public MainWindow(IMenuManager menuManager)
+        {
+            MenuManager = menuManager;
+
+            InitializeComponent();
+        }
+
+        private void MenuManagerButton_Click(object sender, RoutedEventArgs e)
+        {
+            MenuManager.Show();
+        }
+
+        protected override void OnClosing(System.ComponentModel.CancelEventArgs e)
+        {
+            //MenuManager.Close();
+
+            base.OnClosing(e);
+        }
+
+        
+    }
+}
---
+++ WaiterManagement/WaiterClient/Infrastructure/Installers.cs
@@ -0,0 +1,20 @@
+﻿using Ninject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WaiterClient.Abstract;
+using WaiterClient.Concrete;
+
+namespace WaiterClient.Infrastructure
+{
+    class Installers
+    {
+        internal static void ConfigureContainer(out IKernel container)
+        {
+            container = new StandardKernel();
+            container.Bind<IMainWindow>().To<MainWindow>().InSingletonScope();
+        }
+    }
+}
