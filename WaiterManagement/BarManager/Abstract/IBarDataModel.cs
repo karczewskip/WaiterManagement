c22ad23adfa7ffe88084d32bdb6e55ab3643c38a
Pawel <pawel.karczewski@lingaro.com>
2014-10-20 16:59:00 +0200

    Naprawa

---
+++ WaiterManagement/BarManager/Abstract/IBarDataModel.cs
@@ -0,0 +1,17 @@
+﻿using ClassLib.DbDataStructures;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BarManager.Abstract
+{
+    public interface IBarDataModel
+    {
+        IList<MenuItemCategory> GetAllCategories();
+        MenuItemCategory AddCategoryItem(string CategoryName, string CategoryDescription);
+
+        IList<MenuItem> GetAllMenuItems();
+    }
+}
---
+++ WaiterManagement/BarManager/ViewModel/AddCategoryViewModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BarManager.Abstract;
+using System.Windows;
+
+namespace BarManager.ViewModel
+{
+    public class AddCategoryViewModel : IAddCategoryViewModel
+    {
+        private IBarDataModel DataModel;
+        private IMenuManagerViewModel MenuManagerViewModel;
+
+        public string CategoryName { get; set; }
+        public string CategoryDescription { get; set; }
+
+        public AddCategoryViewModel(IBarDataModel dataModel, IMenuManagerViewModel menuManagerViewModel)
+        {
+            DataModel = dataModel;
+            MenuManagerViewModel = menuManagerViewModel;
+        }
+
+        public bool AddCategory()
+        {
+            if (string.IsNullOrEmpty(CategoryName))
+                return false;
+
+            
+
+            if( MenuManagerViewModel.ListOfCategories.Any(cat => cat.Name.Equals(CategoryName)))
+            {
+                MessageBox.Show("There is category named: " + CategoryName);
+                return false;
+            }
+
+            var AddingCategory = DataModel.AddCategoryItem(CategoryName, CategoryDescription);
+            if (AddingCategory != null)
+            {
+                MenuManagerViewModel.ListOfCategories.Add(AddingCategory);
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
