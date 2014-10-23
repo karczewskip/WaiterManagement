1ccf17f79c3a10a287c51760417da45dfe451b2f
Pawel <pawel.karczewski@lingaro.com>
2014-10-24 01:08:56 +0200

    Waiter Manager2

---
+++ WaiterManagement/BarManager/View/AddWaiterWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using BarManager.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace BarManager.View
+{
+    /// <summary>
+    /// Interaction logic for AddWaiterWindow.xaml
+    /// </summary>
+    public partial class AddWaiterWindow : Window, IAddWaiterWindow
+    {
+        private IAddWaiterViewModel AddWaiterViewModel;
+
+        public AddWaiterWindow(IAddWaiterViewModel addWaiterViewModel)
+        {
+            AddWaiterViewModel = addWaiterViewModel;
+
+            this.DataContext = AddWaiterViewModel;
+
+            InitializeComponent();
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (AddWaiterViewModel.AddWaiter())
+                Close();
+        }
+
+        protected override void OnClosing(System.ComponentModel.CancelEventArgs e)
+        {
+            e.Cancel = true;
+            this.Hide();
+        }
+    }
+}
