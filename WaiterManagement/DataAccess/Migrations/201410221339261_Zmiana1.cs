25360fb37c15765e35d47b80f674d285fa02a3d0
ILoveCamelCasing <davidcvpl@gmail.com>
2014-10-28 15:01:40 +0100

    Zmiana w zmiana1

--- WaiterManagement/DataAccess/Migrations/201410221339261_Zmiana1.cs
+++ WaiterManagement/DataAccess/Migrations/201410221339261_Zmiana1.cs
@@ -7,41 +7,41 @@ namespace DataAccess.Migrations
     {
         public override void Up()
         {
-            CreateTable(
-                "dbo.MenuItemCategories",
-                c => new
-                    {
-                        Id = c.Int(nullable: false, identity: true),
-                        Name = c.String(),
-                        Description = c.String(),
-                    })
-                .PrimaryKey(t => t.Id);
+            //CreateTable(
+            //    "dbo.MenuItemCategories",
+            //    c => new
+            //        {
+            //            Id = c.Int(nullable: false, identity: true),
+            //            Name = c.String(),
+            //            Description = c.String(),
+            //        })
+            //    .PrimaryKey(t => t.Id);
             
-            CreateTable(
-                "dbo.MenuItems",
-                c => new
-                    {
-                        Id = c.Int(nullable: false, identity: true),
-                        Name = c.String(),
-                        Description = c.String(),
-                        Price_Amount = c.Single(nullable: false),
-                        Price_Currency = c.String(),
-                        Category_Id = c.Int(),
-                    })
-                .PrimaryKey(t => t.Id)
-                .ForeignKey("dbo.MenuItemCategories", t => t.Category_Id)
-                .Index(t => t.Category_Id);
+            //CreateTable(
+            //    "dbo.MenuItems",
+            //    c => new
+            //        {
+            //            Id = c.Int(nullable: false, identity: true),
+            //            Name = c.String(),
+            //            Description = c.String(),
+            //            Price_Amount = c.Single(nullable: false),
+            //            Price_Currency = c.String(),
+            //            Category_Id = c.Int(),
+            //        })
+            //    .PrimaryKey(t => t.Id)
+            //    .ForeignKey("dbo.MenuItemCategories", t => t.Category_Id)
+            //    .Index(t => t.Category_Id);
             
-            CreateTable(
-                "dbo.Orders",
-                c => new
-                    {
-                        Id = c.Int(nullable: false, identity: true),
-                        UserId = c.Int(nullable: false),
-                        WaiterId = c.Int(nullable: false),
-                        TableId = c.Int(nullable: false),
-                    })
-                .PrimaryKey(t => t.Id);
+            //CreateTable(
+            //    "dbo.Orders",
+            //    c => new
+            //        {
+            //            Id = c.Int(nullable: false, identity: true),
+            //            UserId = c.Int(nullable: false),
+            //            WaiterId = c.Int(nullable: false),
+            //            TableId = c.Int(nullable: false),
+            //        })
+            //    .PrimaryKey(t => t.Id);
             
             CreateTable(
                 "dbo.MenuItemQuantities",
@@ -56,42 +56,42 @@ namespace DataAccess.Migrations
                 .ForeignKey("dbo.Orders", t => t.Order_Id)
                 .Index(t => t.Order_Id);
             
-            CreateTable(
-                "dbo.Tables",
-                c => new
-                    {
-                        Id = c.Int(nullable: false, identity: true),
-                        Number = c.Int(nullable: false),
-                        Description = c.String(),
-                    })
-                .PrimaryKey(t => t.Id);
+            //CreateTable(
+            //    "dbo.Tables",
+            //    c => new
+            //        {
+            //            Id = c.Int(nullable: false, identity: true),
+            //            Number = c.Int(nullable: false),
+            //            Description = c.String(),
+            //        })
+            //    .PrimaryKey(t => t.Id);
             
-            CreateTable(
-                "dbo.WaiterContexts",
-                c => new
-                    {
-                        Id = c.Int(nullable: false, identity: true),
-                        FirstName = c.String(),
-                        LastName = c.String(),
-                        Login = c.String(),
-                        Password = c.String(),
-                    })
-                .PrimaryKey(t => t.Id);
+            //CreateTable(
+            //    "dbo.WaiterContexts",
+            //    c => new
+            //        {
+            //            Id = c.Int(nullable: false, identity: true),
+            //            FirstName = c.String(),
+            //            LastName = c.String(),
+            //            Login = c.String(),
+            //            Password = c.String(),
+            //        })
+            //    .PrimaryKey(t => t.Id);
             
         }
         
         public override void Down()
         {
             DropForeignKey("dbo.MenuItemQuantities", "Order_Id", "dbo.Orders");
-            DropForeignKey("dbo.MenuItems", "Category_Id", "dbo.MenuItemCategories");
+            //DropForeignKey("dbo.MenuItems", "Category_Id", "dbo.MenuItemCategories");
             DropIndex("dbo.MenuItemQuantities", new[] { "Order_Id" });
-            DropIndex("dbo.MenuItems", new[] { "Category_Id" });
-            DropTable("dbo.WaiterContexts");
-            DropTable("dbo.Tables");
+            //DropIndex("dbo.MenuItems", new[] { "Category_Id" });
+            //DropTable("dbo.WaiterContexts");
+            //DropTable("dbo.Tables");
             DropTable("dbo.MenuItemQuantities");
-            DropTable("dbo.Orders");
-            DropTable("dbo.MenuItems");
-            DropTable("dbo.MenuItemCategories");
+            //DropTable("dbo.Orders");
+            //DropTable("dbo.MenuItems");
+            //DropTable("dbo.MenuItemCategories");
         }
     }
 }
