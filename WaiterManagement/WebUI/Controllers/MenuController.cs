f4ae735554b36b3735b68f6f499c2b79951d4ff9
Pawel <karczewskipawel93@gmail.com>
2015-01-25 23:26:26 +0100

    Dodano stronicowanie

--- WaiterManagement/WebUI/Controllers/MenuController.cs
+++ WaiterManagement/WebUI/Controllers/MenuController.cs
@@ -1,4 +1,5 @@
-﻿using System.Web.Mvc;
+﻿using System.Linq;
+using System.Web.Mvc;
 using DataAccess;
 
 namespace WebUI.Controllers
@@ -6,15 +7,16 @@ namespace WebUI.Controllers
     public class MenuController : Controller
     {
         private readonly IBaseDataAccess _baseDataAccess;
+        public int PageSize = 4;
 
         public MenuController(IBaseDataAccess baseDataAccess)
         {
             _baseDataAccess = baseDataAccess;
         }
 
-        public ViewResult List()
+        public ViewResult List(int page = 1)
         {
-            return View(_baseDataAccess.GetMenuItems());
+            return View(_baseDataAccess.GetMenuItems().OrderBy(m => m.Id).Skip((page - 1) * PageSize).Take(PageSize));
         }
     }
 }
