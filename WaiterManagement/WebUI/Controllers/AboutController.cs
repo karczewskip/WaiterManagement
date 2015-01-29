ab2f3d382c34086b272f49eb254e3ac45931e417
Pawel <karczewskipawel93@gmail.com>
2015-01-29 12:46:00 +0100

    Główny header zmieniony

---
+++ WaiterManagement/WebUI/Controllers/AboutController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI.WebControls;
+
+namespace WebUI.Controllers
+{
+    public class AboutController : Controller
+    {
+        //
+        // GET: /About/
+
+        public ViewResult Index()
+        {
+            return View();
+        }
+
+        public ViewResult Contact()
+        {
+            return View();
+        }
+
+    }
+}
