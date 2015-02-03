151cafa4e5e1190f127eb19c22940639a00852c0
Pawel <karczewskipawel93@gmail.com>
2015-02-03 02:33:32 +0100

    Zarys logowania

---
+++ WaiterManagement/WebUI/Infrastructure/Abstract/IAuthProvider.cs
@@ -0,0 +1,11 @@
+﻿namespace WebUI.Infrastructure.Abstract
+{
+    public interface IAuthProvider
+    {
+        string UserName { get; }
+
+        bool IsLogged { get; }
+
+        bool Authenticate(string username, string password); 
+    }
+}
---
+++ WaiterManagement/WebUI/Infrastructure/Concrete/FormsAuthProvider.cs
@@ -0,0 +1,51 @@
+﻿using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using ClassLib.DataStructures;
+using DataAccess;
+using WebUI.Infrastructure.Abstract;
+
+namespace WebUI.Infrastructure.Concrete
+{
+    public class FormsAuthProvider : IAuthProvider
+    {
+        private readonly IClientDataAccess _clientDataAccess;
+        private readonly IManagerDataAccess _managerDataAccess;
+
+        private UserContext _userContext;
+        private System.Web.HttpCookie _authenticationCookie;
+
+        public string UserName
+        {
+            get { return HttpContext.Current.User.Identity.Name; }
+        }
+
+        public bool IsLogged
+        {
+            get { return !string.IsNullOrEmpty(_authenticationCookie.Value); }
+        }
+
+        public FormsAuthProvider(IClientDataAccess clientDataAccess, IManagerDataAccess managerDataAccess)
+        {
+            _clientDataAccess = clientDataAccess;
+            _managerDataAccess = managerDataAccess;
+
+            _authenticationCookie = FormsAuthentication.GetAuthCookie(FormsAuthentication.FormsCookieName, false);
+        }
+
+        public bool Authenticate(string username, string password)
+        {
+            _userContext = _managerDataAccess.LogIn(username, HashClass.CreateFirstHash(password, username));
+
+            if (_userContext != null)
+            {
+                FormsAuthentication.SetAuthCookie(username,false);
+                _authenticationCookie = FormsAuthentication.GetAuthCookie(FormsAuthentication.FormsCookieName, false);
+                
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
